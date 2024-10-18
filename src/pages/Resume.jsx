c9705bf4f798fa02e62121b5d513314cc0d725ff
vluu11 <luuvuong11@gmail.com>
2024-10-18 00:44:26 -0700

    added error.jsx and resume.jsx

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div className="container p-5">
+      <h1 className="text-start">Resume</h1>
+      <h4 className="text-start">Front-end Proficiencies</h4>
+      <ul className="text-start">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h4 className="text-start">Back-end Proficiencies</h4>
+      <ul className="text-start">
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+      </ul>
+    </div>
+  );
+}
